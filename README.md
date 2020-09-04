38f0df55f8b6851655c387911024c0e3b7a6094d
soumya <example@example.com>
2020-09-04 16:45:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PortFolio-1
+Created with CodeSandbox
---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+  imagebaseurl: "/",
+  name: "Soumya Mokashi",
+  role: "Frontend Developer",
+  "linkedinId":"Your LinkedIn Id",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies.",
+  socialLinks: [
+    {
+      name: "github",
+      url: "http://github.com/",
+      className: "fa fa-github"
+    },
+    {
+      "name":"linkedin",
+      "url":"https://www.linkedin.com/",
+      "className":"fa fa-linkedin"
+    }
+  ],
+  aboutme:
+    "I have finished my B.E. in Electronics and Telecommunication. I am interested in learning new technologies also I like to listen to music in my free time.",
+  address: "India",
+  education: [
+    {
+      UniversityName: "Mumbai University",
+      Qualification: "SSC",
+      MonthOfPassing: "June",
+      YearOfPassing: "2015"
+    },
+    {
+      UniversityName: "Mumbai University",
+      Qualification: "HSC",
+      MonthOfPassing: "June",
+      YearOfPassing: "2017"
+    },
+    {
+      UniversityName: "Mumbai University",
+      Qualification: "Pursuing B.E.Degree",
+      MonthOfPassing: "May",
+      YearOfPassing: "2021"
+    }
+  ],
+  skillsDescription: "My Skills are",
+  skills: [
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "JavaScript"
+    },
+    {
+      skillname: "C programming"
+    },
+    {
+      skillname: "Java"
+    },
+    {
+      skillname: "Python"
+    },
+    {
+      skillname: "Reactjs"
+    },
+    {
+      skillname: "React Native"
+    },
+    {
+      skillname: "Autocad"
+    },
+    {
+      skillname: "LtSpice"
+    }
+  ],
+  portfolio: [
+    {
+      name: "light detector using NAND gate",
+      description:
+        "The project was to show the aaplication of NAND gate that whenever a light falls on IR Led the LED would glow.",
+      imgurl: "Images1/nandGate.jpg"
+    },
+    {
+      name: "Musically Rhythm",
+      description:
+        "You might have seen the Disco Lights or DJ lights or light during a function that turn ON and OFF according to the beats or Rhythm of the music. This Music Rhythm Operated Dancing Light circuit is based on transistor BC547.",
+      imgurl: "Images1/musicallyrhythm.jpg"
+    },
+    {
+      name: "Facial Recognition based attendance monitoring system.",
+      description:
+        "Face recognition-based attendance system is a process of recognizing the students face for taking attendance by using face biometrics based on high - definition monitor video and other information technology.",
+      imgurl: "Images1/facialRecognition.jpg"
+    },
+    {
+      name: "Quick analysis of detection of food grains. ",
+      description:
+        "In the present grain-handling scenario, grain type and quality are identified manually by visual inspection which is tedious and not accurate. There is need for the growth of fast, accurate and objective system for quality determination of food grains. This project proposes a model that uses color and geometrical features as attributes for classification.",
+      imgurl: "Images1/foodgrain.jpg"
+    }
+  ]
+};
+
+export default resumeData;
